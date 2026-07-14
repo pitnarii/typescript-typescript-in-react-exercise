a9c259e64ffd0615962d75c61e5ae1c6859c02d9
pitnaree <pitnaree_@outlook.com>
2026-07-14 13:50:47 +0700

    feat: add answer to ex.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
   // TODO4 : Render ออกมาใน App.tsx
-  return;
+  return(
+    <Greeting name="Pam" age={20} />
+  );
 }
 
 export default App;
